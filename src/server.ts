24fc414b7f95300dbc3fab1f0d6d193011ce02f0
thonk <hello@villainsrule.xyz>
2025-12-28 22:18:31 -0500

    bump chrome UA vers

--- src/server.ts
+++ src/server.ts
@@ -42,7 +42,7 @@ Bun.serve({
             const didWork = await new Promise((resolve) => {
                 fetch(json.link, {
                     headers: {
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36',
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
                         'Accept-Language': 'en-US,en;q=0.9',
                         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
                         'Referer': json.link
